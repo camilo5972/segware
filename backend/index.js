d97e25796b0209973152c803bc007473af6a0d0e
Camilo Henao <camilo5972@gmail.com>
2019-11-03 17:07:08 -0500

    Support of Websockets to broadcasting Updates

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const app = express();
+const http = require('http');
+const WebSocket = require('ws');
 const routes = require('./routes/post.route');
 
 app.use(bodyParser.urlencoded({extended:false}));
@@ -23,12 +25,32 @@ app.use((req, res, next) => {
 
 app.use('/api', routes);
 
+//initialize a simple http server
+const server = http.createServer(app);
+
 db.connectDB()
     .then(() => {
-        app.listen(port, '0.0.0.0', () => {
+        server.listen(port, '0.0.0.0', () => {
             console.log(`App listening on port ${port}!`);
         });
     })
     .catch(console.log);
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        const data = JSON.parse(message);
+        if (data.action === 'update list') {
+            wss.clients.forEach((client) => {
+                if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    client.send(message);
+                }
+            });
+        }
+        console.log(`Received: ${message}`);
+    });
+});
 
 module.exports = app;
